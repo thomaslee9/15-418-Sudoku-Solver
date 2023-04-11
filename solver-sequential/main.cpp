e6046f711e52db8823fa700a3a481d2c165f33e3
thomaslee9 <tritelta.superior@gmail.com>
2023-04-11 17:40:11 -0400

    initial commit

---
+++ solver-sequential/main.cpp
@@ -0,0 +1,14 @@
+// Thomas Lee Alex Mikhalenko
+// thomasle  amikhale
+// 15-418
+// Final Project
+// Sudoku Solver
+//
+// solver-sequential
+
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+    
+}
