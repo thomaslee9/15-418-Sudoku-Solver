c3205b8348c41c7d3897788fd6de7b81a74eb1be
thomaslee9 <tritelta.superior@gmail.com>
2023-05-05 01:46:47 -0400

    final commit 1

--- solver-threaded/solver-thread.cpp
+++ solver-threaded/solver-thread.cpp
@@ -155,7 +155,7 @@ int main(int argc, char** argv) {
     bool solved = false;
     std::stack<board> boardStack;
     omp_init_lock (&stackLock);
-    std::string filename = "test-medium-0.txt";
+    std::string filename = "test-standard-medium.txt";
     loadFromFile(filename, initial);
 
     auto start = std::chrono::steady_clock::now();
